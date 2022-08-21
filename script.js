347cbc1a6852b4245667c1abb0b5002165765466
evanboyd31 <evan.boyd@mail.mcgill.ca>
2022-08-20 20:30:09 -0700

    Add event listeners to each number button so display can become populated with numbers

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+const zeroButton = document.querySelector("#zero");
+const oneButton = document.querySelector("#one");
+const twoButton = document.querySelector("#two");
+const threeButton = document.querySelector("#three");
+const fourButton = document.querySelector("#four");
+const fiveButton = document.querySelector("#five");
+const sixButton = document.querySelector("#six");
+const sevenButton = document.querySelector("#seven");
+const eightButton = document.querySelector("#eight");
+const nineButton = document.querySelector("#nine");
+const displayDiv = document.querySelector(".display");
+
+zeroButton.addEventListener('click', updateDisplay);
+oneButton.addEventListener('click', updateDisplay);
+twoButton.addEventListener('click', updateDisplay);
+threeButton.addEventListener('click', updateDisplay);
+fourButton.addEventListener('click', updateDisplay);
+fiveButton.addEventListener('click', updateDisplay);
+sixButton.addEventListener('click', updateDisplay);
+sevenButton.addEventListener('click', updateDisplay);
+eightButton.addEventListener('click', updateDisplay);
+nineButton.addEventListener('click', updateDisplay);
+
+
+function updateDisplay(e) {
+    if (displayDiv.textContent === "0") {
+        displayDiv.textContent = e.target.textContent;
+    } else {
+        displayDiv.textContent += e.target.textContent;
+    }
+}
+
 function add(num1, num2) {
     return num1 + num2;
 }
